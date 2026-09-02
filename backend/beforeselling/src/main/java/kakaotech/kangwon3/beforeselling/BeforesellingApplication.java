8092c66ba59e15e6879447070e7523b485cffcdd
niceome <gylim7826@naver.com>
2026-09-02 20:32:02 +0900

    chore:백엔드 프로젝트 초기 세팅

---
+++ backend/beforeselling/src/main/java/kakaotech/kangwon3/beforeselling/BeforesellingApplication.java
@@ -0,0 +1,13 @@
+package kakaotech.kangwon3.beforeselling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeforesellingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeforesellingApplication.class, args);
+	}
+
+}
