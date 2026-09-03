bab0ee4f243e365978db2c99452cde412928bd60
hyotatoFrappuccino <hjkiln0@gmail.com>
2026-09-03 20:00:33 +0900

    chore: 프로필별 설정 파일 분리 및 OAuth2/JWT 설정 추가

--- backend/beforeselling/src/main/java/kakaotech/kangwon3/beforeselling/BeforesellingApplication.java
+++ backend/beforeselling/src/main/java/kakaotech/kangwon3/beforeselling/BeforesellingApplication.java
@@ -2,8 +2,10 @@ package kakaotech.kangwon3.beforeselling;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class BeforesellingApplication {
 
 	public static void main(String[] args) {
