65f3d1100e3b5cda7b41010e83389590886155fa
SumiNK03 <0417nam@naver.com>
2026-09-07 21:22:51 +0900

    feature: 로그인 여부에 따른 버튼 클릭 동작 구현

--- frontend/palgijeonae/src/components/common/Header/index.tsx
+++ frontend/palgijeonae/src/components/common/Header/index.tsx
@@ -1,14 +1,24 @@
+import { useNavigate } from 'react-router-dom'
+
 import { useAuthStore } from '../../../store/useAuthStore'
 import logo from '../../../assets/logo.png'
 
 function Header() {
     // const isLoggedIn = useAuthStore((state) => state.isLoggedIn)
     const isLoggedIn = true
+    const navigate = useNavigate()
+
+    const handleButtonClick = () => {
+        navigate(isLoggedIn ? '/mypage' : '/login')
+    }
 
     return (
         <header className="fixed top-0 left-0 z-50 flex w-full items-center justify-between border-b border-neutral-border bg-white px-5 py-4">
             <img src={logo} alt="Logo" className="w-[150px] object-contain" />
-            <button className="flex items-center justify-center rounded-full border border-neutral-border px-8 py-2 text-l font-medium text-neutral-dark">
+            <button
+                className="flex cursor-pointer items-center justify-center rounded-full border border-neutral-border px-8 py-2 text-base font-medium text-neutral-dark"
+                onClick={handleButtonClick}
+            >
                 {isLoggedIn ? '마이페이지' : '로그인'}
             </button>
         </header>
