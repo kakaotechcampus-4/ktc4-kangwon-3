3b672c3f156f50f171cffcfe2255e19611e225ac
SumiNK03 <0417nam@naver.com>
2026-09-07 19:42:53 +0900

    feature: 공통 Header 및 Layout 컴포넌트 추가, 라우트에 적용

--- frontend/palgijeonae/src/App.tsx
+++ frontend/palgijeonae/src/App.tsx
@@ -1,6 +1,6 @@
-import { useState } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
+import Layout from './components/layout/Layout.tsx'
 import MainPage from './pages/Main/index.tsx'
 import LoginPage from './pages/Login/index.tsx'
 import UploadPage from './pages/Upload/index.tsx'
@@ -15,13 +15,15 @@ function App() {
     <div className="">
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<MainPage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/upload" element={<UploadPage />} />
-          <Route path="/question" element={<QuestionPage />} />
-          <Route path="/judgement" element={<JudgementPage />} />
-          <Route path="/result" element={<ResultPage />} />
-          <Route path="/mypage" element={<MyPage />} />
+          <Route element={<Layout />}>
+            <Route path="/" element={<MainPage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/upload" element={<UploadPage />} />
+            <Route path="/question" element={<QuestionPage />} />
+            <Route path="/judgement" element={<JudgementPage />} />
+            <Route path="/result" element={<ResultPage />} />
+            <Route path="/mypage" element={<MyPage />} />
+          </Route>
         </Routes>
       </BrowserRouter>
     </div>
