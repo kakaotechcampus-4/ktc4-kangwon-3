54bf48e938a4450fe4c7ba01ab7e0f1ad5c33c98
SumiNK03 <0417nam@naver.com>
2026-09-07 19:33:26 +0900

    feature: 페이지별 라우팅 연결 (헤더 제외)

--- frontend/palgijeonae/src/App.tsx
+++ frontend/palgijeonae/src/App.tsx
@@ -1,13 +1,27 @@
 import { useState } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
+import MainPage from './pages/Main/index.tsx'
+import LoginPage from './pages/Login/index.tsx'
+import UploadPage from './pages/Upload/index.tsx'
+import QuestionPage from './pages/Question/index.tsx'
+import JudgementPage from './pages/Judgment/index.tsx'
+import ResultPage from './pages/Result/index.tsx'
+import MyPage from './pages/Mypage/index.tsx'
+
 function App() {
 
   return (
     <div className="">
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<div>Home</div>} />
+          <Route path="/" element={<MainPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/upload" element={<UploadPage />} />
+          <Route path="/question" element={<QuestionPage />} />
+          <Route path="/judgement" element={<JudgementPage />} />
+          <Route path="/result" element={<ResultPage />} />
+          <Route path="/mypage" element={<MyPage />} />
         </Routes>
       </BrowserRouter>
     </div>
