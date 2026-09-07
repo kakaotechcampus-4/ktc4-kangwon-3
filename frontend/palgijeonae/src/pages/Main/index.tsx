17b53c6e78104f6395beff77f18b23c4f1142172
SumiNK03 <0417nam@naver.com>
2026-09-07 21:54:54 +0900

    feature: 로그인 상태 여부 가져오기

--- frontend/palgijeonae/src/pages/Main/index.tsx
+++ frontend/palgijeonae/src/pages/Main/index.tsx
@@ -1,4 +1,9 @@
+import { useAuthStore } from "../../store/useAuthStore.ts"
+
 function MainPage() {
+    // const isLoggedIn = useAuthStore((state) => state.isLoggedIn);
+    const isLoggedIn = true
+
     return (
         <div>
             <h1>메인 페이지 입니다.</h1>
