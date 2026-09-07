8855b261b8e9c07b469505eeba7ac6192e617989
SumiNK03 <0417nam@naver.com>
2026-09-08 02:41:59 +0900

    style: 메인페이지 및 정보 카드 스타일링과 버튼 스타일 수정

--- frontend/palgijeonae/src/components/common/Button/index.tsx
+++ frontend/palgijeonae/src/components/common/Button/index.tsx
@@ -9,7 +9,7 @@ function Button({ text, onClick, fontSize = 20 }: ButtonProps) {
         <button
             type="button"
             onClick={onClick}
-            className="flex cursor-pointer items-center justify-center gap-2.5 rounded-[10px] bg-primary px-[25px] py-[14px] font-extrabold leading-[1.2] text-center text-white"
+            className="flex cursor-pointer items-center justify-center gap-2.5 rounded-[10px] bg-primary px-[25px] py-[14px] font-bold leading-[1.2] text-center text-white"
             style={{ fontSize: `${fontSize}px` }}
         >
             {text}
