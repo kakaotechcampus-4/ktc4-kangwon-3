d2005d5e474e3207d95e29535eb0fa1a7c20a5df
SumiNK03 <0417nam@naver.com>
2026-09-09 20:15:47 +0900

    style: 로그인 버튼 색상/크기 조정 및 레이아웃 가운데 정렬 수정
    (cherry picked from commit c1a6abd19b0e047518d28be72017909ea2e251b0)

--- frontend/palgijeonae/src/pages/Login/LoginButton.tsx
+++ frontend/palgijeonae/src/pages/Login/LoginButton.tsx
@@ -10,11 +10,11 @@ interface LoginButtonProps {
 function LoginButton({ type, image, BorderColor, BackgroundColor, textColor, onClick }: LoginButtonProps) {
     return (
         <button
-            className={`flex w-full flex-row items-center justify-center gap-2 rounded-10 border-[1px] border-${BorderColor} bg-${BackgroundColor} py-3 px-4`}
+            className={`flex flex-row items-center justify-center gap-5 self-stretch rounded-xl border ${BorderColor} ${BackgroundColor} py-2.5 pr-8 pl-5`}
             onClick={onClick}
         >
-            <img src={image} alt={`${type} logo`} className="h-6 w-6 shrink-0 object-contain" />
-            <span className={`text-base font-semibold leading-4.5 text-${textColor}`}>{type} 계정으로 로그인</span>
+            <img src={image} alt={`${type} logo`} className="h-9 w-9 shrink-0 rounded-[10px] object-cover" />
+            <span className={`text-lg leading-7 font-semibold ${textColor}`}>{type} 계정으로 로그인</span>
         </button>
     )
 }
