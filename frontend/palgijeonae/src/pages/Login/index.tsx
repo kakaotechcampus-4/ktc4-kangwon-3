86de4dfba4de6b24f70f3ee11b55e8b4adbda3f2
SumiNK03 <0417nam@naver.com>
2026-09-09 19:41:12 +0900

    feature: 로그인 버튼 구성 및 로그인 버튼 export 추가
    (cherry picked from commit 9f3c17d8b3909be209a8baacced932a49a9b10f9)

--- frontend/palgijeonae/src/pages/Login/index.tsx
+++ frontend/palgijeonae/src/pages/Login/index.tsx
@@ -1,7 +1,42 @@
+import kakao from "../../assets/login-kakao.png"
+import naver from "../../assets/login-naver.png"
+import google from "../../assets/login-google.png"
+
+import LoginButton from "./LoginButton.tsx"
+
 function LoginPage() {
     return (
         <div>
-            <h1>로그인 페이지 입니다.</h1>
+            <LoginButton
+                type="카카오"
+                image={kakao}
+                BorderColor="gray-300"
+                BackgroundColor="yellow-400"
+                textColor="black"
+                onClick={() => {
+                    // 카카오 로그인 로직
+                }}
+            />
+            <LoginButton
+                type="네이버"
+                image={naver}
+                BorderColor="green-500"
+                BackgroundColor="green-500"
+                textColor="white"
+                onClick={() => {
+                    // 네이버 로그인 로직
+                }}
+            />
+            <LoginButton
+                type="구글"
+                image={google}
+                BorderColor="gray-300"
+                BackgroundColor="white"
+                textColor="black"
+                onClick={() => {
+                    // 구글 로그인 로직
+                }}
+            />
         </div>
     );
 }
