57278fcb399040b24fdfe672610644b13b48a24a
niceome <gylim7826@naver.com>
2026-09-10 23:09:17 +0900

    feat: ProcessingStatus,ResultStatus 구현

--- backend/beforeselling/src/main/java/kakaotech/kangwon3/beforeselling/domains/diagnoses/domain/entity/ResultStatus.java
+++ backend/beforeselling/src/main/java/kakaotech/kangwon3/beforeselling/domains/diagnoses/domain/entity/ResultStatus.java
@@ -1,4 +1,9 @@
 package kakaotech.kangwon3.beforeselling.domains.diagnoses.domain.entity;
 
 public enum ResultStatus {
+    APPLICABLE,
+    PASSED,
+    REQUIRES_CHECK,
+    CANNOT_DETERMINE,
+    EXCLUDED
 }
