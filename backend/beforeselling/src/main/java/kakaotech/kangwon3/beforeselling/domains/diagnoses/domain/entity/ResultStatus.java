e5446c798c5f2df6d799fc55685c67098301389e
niceome <gylim7826@naver.com>
2026-09-10 23:38:08 +0900

    remove: ResultStatus enum 필드 제거
    (cherry picked from commit a22cc048deb1c0d728458fcbdfeee1001e971a3a)

--- backend/beforeselling/src/main/java/kakaotech/kangwon3/beforeselling/domains/diagnoses/domain/entity/ResultStatus.java
+++ backend/beforeselling/src/main/java/kakaotech/kangwon3/beforeselling/domains/diagnoses/domain/entity/ResultStatus.java
@@ -1,9 +1,5 @@
 package kakaotech.kangwon3.beforeselling.domains.diagnoses.domain.entity;
 
 public enum ResultStatus {
-    APPLICABLE,
-    PASSED,
-    REQUIRES_CHECK,
-    CANNOT_DETERMINE,
-    EXCLUDED
+
 }
