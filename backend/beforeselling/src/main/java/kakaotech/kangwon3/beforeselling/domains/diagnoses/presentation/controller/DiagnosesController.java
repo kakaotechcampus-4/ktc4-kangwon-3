f7588bfea8422cc81b75962e1abe22be25e6ff14
niceome <gylim7826@naver.com>
2026-09-11 16:09:11 +0900

    feat: DiagnosesSortType 구현(생성시간 기준 오름차순,내림차순)
    (cherry picked from commit f57b44af013f20d84b8dc0c4d09e67551a3db7db)

--- backend/beforeselling/src/main/java/kakaotech/kangwon3/beforeselling/domains/diagnoses/presentation/controller/DiagnosesController.java
+++ backend/beforeselling/src/main/java/kakaotech/kangwon3/beforeselling/domains/diagnoses/presentation/controller/DiagnosesController.java
@@ -1,4 +1,9 @@
 package kakaotech.kangwon3.beforeselling.domains.diagnoses.presentation.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
 public class DiagnosesController {
 }
