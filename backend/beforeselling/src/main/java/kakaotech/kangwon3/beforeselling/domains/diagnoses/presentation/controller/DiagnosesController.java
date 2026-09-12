56d81b230b9627a8d408b8bb8b3429bd9ca55efc
niceome <gylim7826@naver.com>
2026-09-12 19:40:04 +0900

    feat: GlobalExceptionHandler에 page,size 관련 검증 에러 변환 (5xx -> 4xx)
    (cherry picked from commit 6b3f2cafb54a99a1c32e0719e9783432478e5830)

--- backend/beforeselling/src/main/java/kakaotech/kangwon3/beforeselling/domains/diagnoses/presentation/controller/DiagnosesController.java
+++ backend/beforeselling/src/main/java/kakaotech/kangwon3/beforeselling/domains/diagnoses/presentation/controller/DiagnosesController.java
@@ -17,10 +17,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
+@Validated
 @RequestMapping("/api/v1/diagnoses")
 @RequiredArgsConstructor
 public class DiagnosesController implements DiagnosesApi {
