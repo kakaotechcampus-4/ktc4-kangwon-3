2f00d4cc7b043d134d58bef8a4c096a2c4dbd3fc
niceome <gylim7826@naver.com>
2026-09-13 23:49:33 +0900

    fix: 작업과 무관한 User, ErrorCodeController 변경 복구

--- backend/beforeselling/src/main/java/kakaotech/kangwon3/beforeselling/global/controller/ErrorCodeController.java
+++ backend/beforeselling/src/main/java/kakaotech/kangwon3/beforeselling/global/controller/ErrorCodeController.java
@@ -23,7 +23,6 @@ public class ErrorCodeController {
                 buildGroup("공통", CommonResponseCode.values()),
                 buildGroup("인증", AuthResponseCode.values())
         );
-
         model.addAttribute("groups", groups);
         return "error-codes";
     }
