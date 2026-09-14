392429802fa77ff044d257ec83bee65a09b982c9
SumiNK03 <0417nam@naver.com>
2026-09-14 21:30:44 +0900

    feature: 추가된 상품 카드에 삭제 기능 추가
    (cherry picked from commit 5f1eb1c4e30e3aeaf36ca5cfa0a9961f9a0f22f6)

--- frontend/palgijeonae/src/pages/Upload/AddedProduct.tsx
+++ frontend/palgijeonae/src/pages/Upload/AddedProduct.tsx
@@ -3,30 +3,39 @@ import imageIcon from "../../assets/upload-img_txt.svg"
 import DefaultBox from "../../components/common/DefaultBox/index.tsx";
 import toggleIcon from "../../assets/upload-toggle.svg"
 import defaultThumbnail from "../../assets/upload-defaultThumbnail.svg"
+import deleteIcon from "../../assets/delete-gray.svg"
 
 interface AddedProductProps {
     type: "url" | "text/image";
     title: string;
     thumbnail?: string;
+    onRemove: () => void;
 }
 
 
-function AddedProduct({ type, title, thumbnail }: AddedProductProps) {
-    return ( 
+function AddedProduct({ type, title, thumbnail, onRemove }: AddedProductProps) {
+    return (
         <DefaultBox>
             <div className="relative flex w-full gap-5">
-                <img src={thumbnail ? thumbnail : defaultThumbnail} 
-                    alt="상품 썸네일" 
+                <img src={thumbnail ? thumbnail : defaultThumbnail}
+                    alt="상품 썸네일"
                     className="w-20 h-20 object-cover rounded-lg border border-neutral-border" />
                 <div className="flex flex-col gap-3">
                     <div className="flex gap-2 items-center">
-                        <img src={type === "url" ? urlIcon : imageIcon} 
+                        <img src={type === "url" ? urlIcon : imageIcon}
                             alt={type === "url" ? "URL 아이콘" : "이미지 아이콘"}
                             className="w-5 h-5" />
                         <p className="text-base text-neutral-border font-bold">{type === "url" ? "URL" : "이미지 / 텍스트"}</p>
                     </div>
                     <h3 className="text-xl font-bold text-neutral-dark">{title}</h3>
                 </div>
+                <button
+                    type="button"
+                    onClick={onRemove}
+                    className="absolute top-0 right-0 flex h-5 w-5 cursor-pointer items-center justify-center rounded-full bg-white"
+                >
+                    <img src={deleteIcon} alt="삭제" className="h-full w-full" />
+                </button>
                 <img src={toggleIcon} alt="토글 아이콘" className="absolute top-1/2 right-0 h-5 w-5 -translate-y-1/2" />
             </div>
         </DefaultBox>
