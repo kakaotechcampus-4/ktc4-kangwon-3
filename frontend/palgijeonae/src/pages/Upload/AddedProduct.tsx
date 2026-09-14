d2434e95420388a213e59f3c7b84a5374892659b
SumiNK03 <0417nam@naver.com>
2026-09-14 21:03:18 +0900

    style: 추가된 상품 카드(AddedProduct) 레이아웃 및 스타일 수정

--- frontend/palgijeonae/src/pages/Upload/AddedProduct.tsx
+++ frontend/palgijeonae/src/pages/Upload/AddedProduct.tsx
@@ -1,5 +1,8 @@
 import urlIcon from "../../assets/upload-url.svg"
 import imageIcon from "../../assets/upload-img_txt.svg"
+import DefaultBox from "../../components/common/DefaultBox/index.tsx";
+import toggleIcon from "../../assets/upload-toggle.svg"
+import defaultThumbnail from "../../assets/upload-defaultThumbnail.svg"
 
 interface AddedProductProps {
     type: "url" | "text/image";
@@ -10,16 +13,23 @@ interface AddedProductProps {
 
 function AddedProduct({ type, title, thumbnail }: AddedProductProps) {
     return ( 
-        <div className="flex w-full px-2 py-2 gap-4 border border-neutral-border rounded-lg">
-            <img src={thumbnail ? thumbnail : "/default-thumbnail.png"} alt="상품 썸네일" />
-            <div className="flex flex-col gap-2">
-                <div className="flex flex-col gap-1">
-                    <img src={type === "url" ? urlIcon : imageIcon} alt={type === "url" ? "URL 아이콘" : "이미지 아이콘"} />
-                    <p>{type === "url" ? "URL" : "이미지 / 텍스트"}</p>
+        <DefaultBox>
+            <div className="relative flex w-full gap-5">
+                <img src={thumbnail ? thumbnail : defaultThumbnail} 
+                    alt="상품 썸네일" 
+                    className="w-20 h-20 object-cover rounded-lg border border-neutral-border" />
+                <div className="flex flex-col gap-3">
+                    <div className="flex gap-2 items-center">
+                        <img src={type === "url" ? urlIcon : imageIcon} 
+                            alt={type === "url" ? "URL 아이콘" : "이미지 아이콘"}
+                            className="w-5 h-5" />
+                        <p className="text-base text-neutral-border font-bold">{type === "url" ? "URL" : "이미지 / 텍스트"}</p>
+                    </div>
+                    <h3 className="text-xl font-bold text-neutral-dark">{title}</h3>
                 </div>
-                <h3>{title}</h3>
+                <img src={toggleIcon} alt="토글 아이콘" className="absolute top-1/2 right-0 h-5 w-5 -translate-y-1/2" />
             </div>
-        </div>
+        </DefaultBox>
      );
 }
 
