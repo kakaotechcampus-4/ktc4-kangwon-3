83f0205a8bb645d9dbc968f8408501c16edc5aab
hyotatoFrappuccino <hjkiln0@gmail.com>
2026-09-18 12:48:32 +0900

    feature: 비동기 이벤트 처리와 고아 파일 정리 스케줄러 실행을 위한 설정 추가

---
+++ backend/beforeselling/src/main/java/kakaotech/kangwon3/beforeselling/global/config/SchedulingConfig.java
@@ -0,0 +1,9 @@
+package kakaotech.kangwon3.beforeselling.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableScheduling
+public class SchedulingConfig {
+}
