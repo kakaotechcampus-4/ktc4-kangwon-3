12fd3d7837545fb48c18642d5276b55d196bdd1b
hyotatoFrappuccino <hjkiln0@gmail.com>
2026-09-21 23:56:33 +0900

    refactor: 인증 플로우의 사용자 ID 타입을 UUID로 변경
    (cherry picked from commit 5791dac6f5fed107ed567bd0228e6f9886a655bd)

--- backend/beforeselling/src/main/java/kakaotech/kangwon3/beforeselling/global/security/jwt/TokenClaims.java
+++ backend/beforeselling/src/main/java/kakaotech/kangwon3/beforeselling/global/security/jwt/TokenClaims.java
@@ -2,8 +2,10 @@ package kakaotech.kangwon3.beforeselling.global.security.jwt;
 
 import kakaotech.kangwon3.beforeselling.domains.user.domain.entity.Role;
 
+import java.util.UUID;
+
 public record TokenClaims(
-        Long userId,
+        UUID userId,
         Role role,
         TokenType type,
         String jti
